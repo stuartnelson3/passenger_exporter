2d6cbbf8a101dd4a743cf1a11323b1ac6afafc38
stuart nelson <stuartnelson3@gmail.com>
2016-05-10 10:17:20 +0200

    Update parse test
    Update test and add xml sample output.

--- main_test.go
+++ main_test.go
@@ -1,32 +1,30 @@
 package main
 
 import (
-	"encoding/xml"
-	"fmt"
 	"os"
 	"testing"
 )
 
-func TestParseWorks(t *testing.T) {
+func TestParseOutput(t *testing.T) {
 	f, err := os.Open("passenger_xml_output.xml")
 	if err != nil {
 		t.Fatalf("open xml file failed: %v", err)
 	}
 
-	info, err := parse(f)
+	info, err := parseOutput(f)
 	if err != nil {
 		t.Fatalf("parse xml file failed: %v", err)
 	}
 
 	for _, sg := range info.SuperGroups {
-		for _, proc := range sg.Group.Processes {
+		if want, got := "/src/app/my_app", sg.Group.Options.AppRoot; want != got {
+			t.Fatalf("incorrect app_root: wanted %s, got %s", want, got)
+		}
 
-			byts, err := xml.MarshalIndent(proc, "  ", "    ")
-			if err != nil {
-				t.Fatalf("marshal indent process failed: %v", err)
+		for _, proc := range sg.Group.Processes {
+			if want, got := "2254", proc.ProcessGroupID; want != got {
+				t.Fatalf("incorrect process_group_id: wanted %s, got %s", want, got)
 			}
-			fmt.Printf("%v\n", string(byts))
 		}
 	}
-	t.Fail()
 }
